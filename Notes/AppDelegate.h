4d26026c6c5c54cb64e039bcbfbe978da812beff
廖宝裕 <1576660896@qq.com>
2020-07-20 15:31:14 +0800

    property and synthesize

---
+++ Notes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by Civet on 2020/7/20.
+//  Copyright © 2020 PersonalONBYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
